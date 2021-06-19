15be07d96efac4499b152e994fb0024727713aa9
muhammadzourdy <zhafarmuhammad12@gmail.com>
2021-06-19 21:03:30 +0700

    render the products <-- > no hard coded

--- src/js/App.js
+++ src/js/App.js
@@ -13,10 +13,74 @@ const productsDOM = document.querySelector(".products-center");
 let cart = [];
 
 //warn ==> getting the products
-class Products {}
+class Products {
+  async getProducts() {
+    try {
+      const res = await fetch("../../products.json");
+      const data = await res.json();
+
+      if (!res.ok)
+        throw new Error(
+          "Fetching Process Failed 404 ==> your connection is down"
+        );
+
+      let items = data.items;
+
+      items = items.map((item) => {
+        //warn => destructuring
+        const {
+          fields: {
+            image: {
+              fields: {
+                file: { url },
+              },
+            },
+            price,
+            title,
+          },
+          sys: { id },
+        } = item;
+
+        return { url, price, title, id };
+      });
+
+      return items;
+    } catch (error) {
+      console.error(`wait up bruh!, ${error.message}`);
+    }
+  }
+}
 
 //warn ==> display the products
-class UI {}
+class UI {
+  displayProducts(products) {
+    let result = "";
+
+    products.forEach((product) => {
+      result += `
+      <article class="product">
+      <div class="img-container">
+        <img
+          src='${product.url}'
+          alt="product"
+          class="product-img"
+        />
+        <button class="bag-btn" data-id='${product.id}'>
+          <i class="fas fa-shopping-cart"></i>
+          add to bag
+        </button>
+      </div>
+    <h3>${product.title}</h3>
+    <h4>$${product.price}</h4>
+  </article>
+      `;
+    });
+
+    productsDOM.innerHTML = result;
+
+    console.log(products);
+  }
+}
 
 //warn ==> storage
 class Storage {}
@@ -25,5 +89,5 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const ui = new UI();
   const products = new Products();
 
-  console.log(e.target);
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
